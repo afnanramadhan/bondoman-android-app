0504ef359d4884b64cc701a8c8fa133ce0c6faa9
Jauza Lathifah Annassalafi <13521030@std.stei.itb.ac.id>
2024-03-16 22:06:19 +0700

    feat: navbar

--- app/src/main/java/com/example/android_hit/MainActivity.kt
+++ app/src/main/java/com/example/android_hit/MainActivity.kt
@@ -2,10 +2,42 @@ package com.example.android_hit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.android_hit.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setCurrentFragment(Transaction(), HeaderTransaction())
+        binding.bottomNavigation.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.nav_transaction -> {
+                    setCurrentFragment(Transaction(), HeaderTransaction())
+                }
+                R.id.nav_scan -> {
+                    setCurrentFragment(Scan(),  HeaderScan())
+                }
+                R.id.nav_graphs -> {
+                    setCurrentFragment(Graphs(), HeaderGraphs())
+                }
+                R.id.nav_settings -> {
+                    setCurrentFragment(Settings(), HeaderSettings())
+                }
+            }
+            true
+        }
+
     }
+
+    private fun setCurrentFragment(fragment: Fragment, header: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.frame_layout, fragment)
+            replace(R.id.header_layout, header)
+            commit()
+        }
+
 }
