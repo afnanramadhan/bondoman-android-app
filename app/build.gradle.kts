e9a41340808793270e8e76f669d8aca91284b037
syauqijan <13521014@std.stei.itb.ac.id>
2024-03-19 12:42:38 +0700

    fix scan nota

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("kotlin-parcelize")
+    id ("kotlin-parcelize")
 }
 
 android {
@@ -17,8 +17,6 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
-
-//
 
     buildTypes {
         release {
@@ -57,5 +55,4 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
 }
